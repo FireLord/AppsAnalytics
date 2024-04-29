3080d60fffa0995c821843e245ebb4c841b7089a
FireLord <firelord.xda@gmail.com>
2024-04-29 11:01:31 +0530

    Initial Commit

---
+++ AppsAnalytics/AppsAnalytics.h
@@ -0,0 +1,18 @@
+//
+//  AppsAnalytics.h
+//  AppsAnalytics
+//
+//  Created by admin on 29/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppsAnalytics.
+FOUNDATION_EXPORT double AppsAnalyticsVersionNumber;
+
+//! Project version string for AppsAnalytics.
+FOUNDATION_EXPORT const unsigned char AppsAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppsAnalytics/PublicHeader.h>
+
+
